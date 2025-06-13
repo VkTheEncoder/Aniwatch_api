3cb2e2a6078726cdd74bbadbfa3c8616173fa733
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-06-13 23:21:23 +0530

    Create shims-aniwatch.d.ts

---
+++ src/types/shims-aniwatch.d.ts
@@ -0,0 +1,6 @@
+// shims-aniwatch.d.ts
+declare module "aniwatch" {
+  // Minimal typing so your imports work; you can expand this
+  // if you want actual types for HiAnime.Scraper, etc.
+  export const HiAnime: any;
+}
